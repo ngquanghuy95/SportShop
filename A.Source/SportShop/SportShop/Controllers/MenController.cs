8052e73b2d4036ab75db7d45eb154908b6166508
DoanVd <github95@gmail.com>
2016-06-07 01:01:26 -0700

    Doanvd

--- A.Source/SportShop/SportShop/Controllers/MenController.cs
+++ A.Source/SportShop/SportShop/Controllers/MenController.cs
@@ -1,4 +1,5 @@
 ﻿using SportShop.DAO;
+using SportShop.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,38 @@ namespace SportShop.Controllers
     public class MenController : Controller
     {
         // GET: Men
-        public ActionResult Index()
+        APIs api = new APIs();
+        private MyData db = new MyData();
+        public ActionResult Index(int brand)
         {
             ShoppingCart objCart = (ShoppingCart)Session["Cart"];
+            IQueryable<Product> lstProduct = from s in db.Products where s.CategoryID == 2 select s;
+            if (brand != 0)
+            {
+                lstProduct = from s in db.Products where s.CategoryID == 2 && s.BrandID == brand select s;
+            }
+            ViewData["Product"] = lstProduct;
+            ViewData["Brand"] = api.getLstBrand();
+            ViewData["Color"] = api.getLstColor();
+            ViewData["Type"] = api.getLstType();
             return View();
+        }
+        public ActionResult Filter(int BrandID, int TypeShoesID, int ColorID)
+        {
+            IQueryable<Product> lstProduct = from s in db.Products where s.CategoryID == 2 select s;
+            if (BrandID != 1)
+            {
+                lstProduct = from s in lstProduct where s.BrandID == BrandID select s;
+            }
+            if (TypeShoesID != 1)
+            {
+                lstProduct = from s in lstProduct where s.TypeID == TypeShoesID select s;
+            }
+            if (ColorID != 1)
+            {
+                lstProduct = from s in lstProduct where s.ColorID == ColorID select s;
+            }
+            return PartialView("ListProduct", lstProduct);
         }
     }
 }
--- A.Source/SportShop/SportShop/Controllers/WomanController.cs
+++ A.Source/SportShop/SportShop/Controllers/WomanController.cs
@@ -14,16 +14,36 @@ namespace SportShop.Controllers
         // GET: Woman
         APIs api = new APIs();
         private MyData db = new MyData();
-        public ActionResult Index()
+        public ActionResult Index(int brand)
         {
-            ViewData["lstP"] = api.getLstProduct(2);
-            ViewData["lstNP"] = api.getLstNewProduct(2);
-            ViewData["lstSP"] = api.getLstSaleProduct(2);
-            ViewData["lstRP"] = api.getLstCommonProduct(2);
+            ShoppingCart objCart = (ShoppingCart)Session["Cart"];
+            IQueryable<Product> lstProduct = from s in db.Products where s.CategoryID == 3 select s;
+            if (brand != 0)
+            {
+                lstProduct = from s in db.Products where s.CategoryID == 3 && s.BrandID == brand select s;
+            }
+            ViewData["Product"] = lstProduct;
             ViewData["Brand"] = api.getLstBrand();
             ViewData["Color"] = api.getLstColor();
             ViewData["Type"] = api.getLstType();
             return View();
+        }
+        public ActionResult Filter(int BrandID, int TypeShoesID, int ColorID)
+        {
+            IQueryable<Product> lstProduct = from s in db.Products where s.CategoryID == 3  select s;
+            if (BrandID != 1)
+            {
+                lstProduct = from s in lstProduct where s.BrandID == BrandID select s;
+            }
+            if (TypeShoesID != 1)
+            {
+                lstProduct = from s in lstProduct where s.TypeID == TypeShoesID select s;
+            }
+            if (ColorID != 1)
+            {
+                lstProduct = from s in lstProduct where s.ColorID == ColorID select s;
+            }
+            return PartialView("ListProduct",lstProduct);
         }
     }
 }
